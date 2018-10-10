e9efd6b3578d4cb4f37622cef38741c8cc64b5f2
Jay Stiles <JayStiles@quickenloans.com>
2018-10-10 11:15:39 -0400

    Adding lab 5

---
+++ GC.Lab_5/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC.Lab_5
+{
+    class Program
+    {
+        public static long uNum { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factoral calculator");
+            //set var
+            string userNumber;
+            int x;
+            long factorial;
+            string check;
+
+            while (true)
+            {
+                //get int
+                Console.Write("Enter an integer: ");
+                userNumber = Console.ReadLine();
+                x = int.Parse(userNumber);
+
+                factorial = getFactorial(x);
+
+                Console.WriteLine("Factorial of " + userNumber + " is " + factorial);
+
+                //Check
+                Console.Write("Continue y/n: ");
+                check = Console.ReadLine();
+                if (check != "y")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                }
+            }
+
+        }
+
+        static long getFactorial(int x)
+        {
+            if (x <= 1)
+            {
+                return 1;
+            }
+            {
+                return (x * getFactorial(x - 1));
+            }
+           
+        }
+
+    }
+}
